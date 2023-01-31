36fb270e0243da49b4f108e476678ba5f5a98eb2
itsnurdy <sergiumer@gmail.com>
2023-01-31 23:18:30 +0000

    Rearannged the application, fully functional now. More design work needed.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 import customtkinter
 import requests
 from pytube import YouTube
-from PIL import Image
+from PIL import Image, ImageTk
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -31,32 +31,32 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
 
         # Thumbnail of link.
-        self.thumbnailImage = customtkinter.CTkImage(Image.open("images/thumbnail.png"), size=(192, 144))
-        self.thumbnail = customtkinter.CTkLabel(self, text="", image=self.thumbnailImage, bg_color="white")
+        self.thumbnailImage = customtkinter.CTkImage(Image.open("images/thumbnail.png"), size=(352, 240))
+        self.thumbnail = customtkinter.CTkLabel(self, text="", image=self.thumbnailImage)
         self.thumbnail.grid(row=0, column=0, columnspan=2)
 
         # Text bard on the app.
-        self.text = customtkinter.CTkLabel(self, text="Insert a youtube video url link.", bg_color="black")
-        self.text.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+        self.text = customtkinter.CTkLabel(self, text="Insert a youtube video url link.")
+        self.text.grid(row=1, column=0, columnspan=2)
 
         # Link input
         self.url_var = tk.StringVar()
-        self.link = customtkinter.CTkEntry(self, width=350, height=40, textvariable=self.url_var, bg_color="white")
-        self.link.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+        self.link = customtkinter.CTkEntry(self, width=400, height=40, textvariable=self.url_var)
+        self.link.grid(row=2, column=0, columnspan=2)
 
         # Main button.
-        self.button = customtkinter.CTkButton(self, text="Select Video", command=self.info, height=40, width=250, bg_color="white")
-        self.button.grid(row=3, column=0, padx=10)
+        self.button = customtkinter.CTkButton(self, text="Select Video", command=self.info, height=40, width=100)
+        self.button.grid(row=3, column=0, padx=(200, 20), sticky="ew")
 
         # Download location button.
         self.directoryImage = customtkinter.CTkImage(Image.open("images/folder.png"), size=(30, 30))
-        self.locationButton = customtkinter.CTkButton(self, text="", image=self.directoryImage, command=self.downloadLocation, height=30, width=40, bg_color="white")
-        self.locationButton.grid(row=3, column=1, padx=10)
+        self.locationButton = customtkinter.CTkButton(self, text="", image=self.directoryImage, command=self.downloadLocation, height=30, width=40)
+        self.locationButton.grid(row=3, column=1, padx=(0, 200), sticky="ew")
 
         # Progress percentage
         self.progressBar = customtkinter.CTkProgressBar(self, height=10, width=500)
         self.progressBar.set(0)
-        self.progressBar.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+        self.progressBar.grid(row=4, column=0, columnspan=2, padx=40, sticky="ew")
 
     # Get YouTube video information.
     def info(self):
@@ -67,14 +67,14 @@ class App(customtkinter.CTk):
             # Changes title to YouTube video.
             self.text.configure(text=self.ytObject.title)
             # Changes thumbnail to YouTube video thumbnail.
-            self.downloadedThumbnail = customtkinter.CTkImage(Image.open(requests.get(self.ytObject.thumbnail_url, stream=True).raw), size=(192, 144))
+            self.downloadedThumbnail = customtkinter.CTkImage(Image.open(requests.get(self.ytObject.thumbnail_url, stream=True).raw), size=(352, 240))
             self.thumbnail.configure(image=self.downloadedThumbnail)
             # Changes the button to now allow downloading.
-            self.button.configure(text="Download", command=self.download, text_color="white")
+            self.button.configure(text="Download", command=self.download)
             # Reset the finished label after checking another link.
         except:
             # Error checking if link is invalid.
-            self.button.configure(text="Invalid Link.", text_color="red")
+            self.text.configure("Invalid Link.", text_color="red")
 
     # Selecting download location.
     def downloadLocation(self):
@@ -83,11 +83,20 @@ class App(customtkinter.CTk):
     # Download function defined to download YouTube video.
     def download(self):
         try:
-            self.video = self.ytObject.streams.get_lowest_resolution()
+            # We retrieve the highest resolution.
+            self.video = self.ytObject.streams.get_highest_resolution()
             self.video.download(self.location)
-            self.button.configure(text="Download completed.", text_color="green")
+            self.button.configure(text="Reset application.", command=self.resetButton)
         except:
-            self.button.configure(text="Invalid Link.", text_color="red")
+            self.text.configure("Invalid Link.", text_color="red")
+
+    def resetButton(self):
+        # It resets every aspect of the app to default values.
+        self.text.configure(text="Insert a youtube video url link.", text_color="white")
+        self.thumbnail.configure(image=self.thumbnailImage)
+        self.button.configure(text="Select Video", command=self.info)
+        self.progressBar.set(0)
+        self.link.configure(textvariable="")
 
     # This is how the progress bar completion gets calculated.
     def on_progress(self, stream, chunk, bytes_remaining):
